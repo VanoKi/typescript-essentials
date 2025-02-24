74d4951bbaefff0cba5ecd9cec6d5f495b30f4d5
VanoKi <kirsvano@gmail.com>
2025-02-24 20:19:20 +0300

    fix: last tasl

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 function getHiddenCard(card: string, last = 4): string {
     // let arr = Array.from(card)
-    return "*".repeat(card.length - last) + card.slice(card.length - last)
+    return "*".repeat(last) + card.slice(card.length - 4)
 }
 
-console.log(getHiddenCard('2034399002125581'));
+console.log(getHiddenCard('1234123412344321', 2));
