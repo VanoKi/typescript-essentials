0001c1ef64d142c4910d643bca047c09fa0f4fa8
VanoKi <kirsvano@gmail.com>
2025-02-24 20:14:46 +0300

    feat: add nrw task

--- script.ts
+++ script.ts
@@ -1,10 +1,6 @@
-function repeat(text: string, count: number) {
-    let ans: string = ""
-    for (let i = 0; i < count; i++) {
-        ans += text
-        // console.log(i);
-    }
-    return ans
+function getHiddenCard(card: string, last = 4): string {
+    // let arr = Array.from(card)
+    return "*".repeat(card.length - last) + card.slice(card.length - last)
 }
 
-console.log(repeat('wo', 3));
+console.log(getHiddenCard('2034399002125581'));
