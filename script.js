c2e8d862f4a9abca35ef5b8dd67247ac5476a94e
VanoKi <kirsvano@gmail.com>
2025-06-07 17:19:05 +0300

    feat: add methods

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const XMLHttpRequest = require('xhr2')
 let xhr = new XMLHttpRequest()
 xhr.open("GET", 'https://jsonplaceholder.typicode.com/posts')
+// xhr.onload =() => {
+//     if (xhr.status === 200) {
+//         console.log(xhr.response)
+//     } else {
+//         console.log(xhr.status)
+//     }
+// }
 xhr.send()
-console.log(xhr)
+
+xhr.onprogress = (e) => {
+    console.log(e)
+}
