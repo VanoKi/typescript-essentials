0edc2dbe6e1f29247555e41722edae847e324a72
VanoKi <kirsvano@gmail.com>
2025-06-11 15:44:59 +0300

    feat: task from interview

--- clousers.js
+++ clousers.js
@@ -1,44 +1,17 @@
-function createGreeting(name) {
-    return function () {
-        console.log('Hi ' + name)
+function createIncrement() {
+    let count = 0
+    function increment() {
+        count++
     }
+    let message = `count is ${count}`
+    function log() {
+        console.log(message)
+    }
+    return [increment, log]
 }
 
-const greetJohn = createGreeting("John");
-greetJohn(); // Привет, John!
-const greetIVan = createGreeting('Ivan')
-greetIVan()
-
-function createCounter() {
-    let state = 0
-    function incVal() {
-        console.log(state++)
-    }
-    return incVal
-}
-
-const counter = createCounter();
-counter(); // 1
-counter(); // 2
-counter(); // 3
-
-function makeFuncs() {
-    let result = [];
-    for (var i = 0; i < 3; i++) {
-        result.push(function() {
-            console.log(i);
-        });
-    }
-    return result;
-}
-
-const funcs = makeFuncs();
-funcs[0](); // ?
-funcs[1](); // ?
-funcs[2](); // ?
-
-const sayOnce = once(() => console.log("Сработало!"));
-
-sayOnce(); // Сработало!
-sayOnce(); // ничего
-sayOnce(); // ничего
+const [increment, log] = createIncrement()
+increment()
+increment()
+increment()
+log()
