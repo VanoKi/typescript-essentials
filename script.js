b64d237d2ebc1eda410ebc4e27355ecf7beee48f
VanoKi <kirsvano@gmail.com>
2025-08-05 17:45:08 +0300

    feat: private

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-function CarCreator(brand, maxSpeed) {
-    this.brand = brand
-    this.maxSpeed = maxSpeed
-    this.startEngine = function () {
+class Car {
+    #brand
+    constructor(brand, maxSpeed) {
+        this.brand = brand
+        this.maxSpeed = maxSpeed
+    }
+    startEngine() {
         console.log(`${this.brand} is started`)
     }
 }
 
-CarCreator.prototype.startEngine = function () {
-    console.log(`${this.brand} is started`)
-}
-
-const bmw = CarCreator('bmw', 200)
-const kia = CarCreator('kia', 220)
-
-bmw.startEngine()
-kia.startEngine()
+const bmw = new Car('bwm', 5)
+console.log(bmw.brand)
