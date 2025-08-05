bffb57d6cd5abcf05cd53b614b1698084c6b720d
VanoKi <kirsvano@gmail.com>
2025-08-05 18:43:27 +0300

    feat: new lesson

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 class Car {
-    #brand
     constructor(brand, maxSpeed) {
         this.brand = brand
         this.maxSpeed = maxSpeed
+        // this.state = {}
     }
     startEngine() {
         console.log(`${this.brand} is started`)
     }
-    get brand() {
-        return this.#brand
-    }
-    set brand(newbrand) {
-        this.#brand = newbrand
-    }
-    static compareCars(car1, car2) {
-        car1.maxSpeed === car2.maxSpeed
-        ? console.log('cars have same speed')
-            : car1.maxSpeed > car2.maxSpeed
-        ? console.log(`${car1.brand} is faster`)
-        : console.log(`${car2.brand} is faster`)
+    stopEngine = () => {
+        console.log(`${this.brand} is stoped`)
     }
 }
 
 
+const car1 = new Car('bmw', 200)
+const car2 = new Car('bmw', 200)
 
+// console.log(car1 === car2)
+// console.log(car1.brand === car2.brand)
+// console.log(car1.startEngine === car2.startEngine)
+// console.log(car1.stopEngine === car2.stopEngine)
+
+// console.log(car1.__proto__ === Car.prototype)
+
+// const str = new String('ello')
+// console.log(str.__proto__ === String.prototype)
+//
+// const str2 = "hello"
+// console.log(Object(str2))
+//
+// const a = 5
+// console.log(a.__proto__ === Number.prototype)
+
+const obj = {}
+console.log(obj.__proto__ === Object.prototype)
