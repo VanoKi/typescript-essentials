5e336dd96f70fe11a1f98996b1b185ab6cb35c73
VanoKi <kirsvano@gmail.com>
2025-08-05 18:02:11 +0300

    feat: static

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ class Car {
     startEngine() {
         console.log(`${this.brand} is started`)
     }
+    get brand() {
+        return this.#brand
+    }
+    set brand(newbrand) {
+        this.#brand = newbrand
+    }
+    static compareCars(car1, car2) {
+        car1.maxSpeed === car2.maxSpeed
+        ? console.log('cars have same speed')
+            : car1.maxSpeed > car2.maxSpeed
+        ? console.log(`${car1.brand} is faster`)
+        : console.log(`${car2.brand} is faster`)
+    }
 }
 
-const bmw = new Car('bwm', 5)
-console.log(bmw.brand)
+
+
