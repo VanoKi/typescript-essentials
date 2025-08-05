fd3b282495203f18101a56c7c57c6d93b4d1e039
VanoKi <kirsvano@gmail.com>
2025-08-05 17:41:32 +0300

    feat: add index.html

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ function CarCreator(brand, maxSpeed) {
 CarCreator.prototype.startEngine = function () {
     console.log(`${this.brand} is started`)
 }
+
+const bmw = CarCreator('bmw', 200)
+const kia = CarCreator('kia', 220)
+
+bmw.startEngine()
+kia.startEngine()
